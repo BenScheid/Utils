05db77dcac7c5c236a966e7af52ed23ad76fb432
BenScheid <bdiehcs@gmail.com>
2025-10-15 19:07:57 +0200

    added proxy

---
+++ proxy/src/main/java/click/scheid/proxy/ProxyController.java
@@ -0,0 +1,47 @@
+package click.scheid.proxy;
+
+
+import java.util.Collections;
+import java.util.logging.Logger;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping(value = "/proxy")
+public class ProxyController {
+	public static final Logger LOGGER = Logger.getLogger(ProxyController.class.getName());
+	private final RestTemplate restTemplate = new RestTemplate();
+
+	@RequestMapping(value = "/**", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+    public ResponseEntity<byte[]> proxy(HttpServletRequest request,
+                                        @RequestBody(required = false) byte[] body) {
+        HttpMethod method = HttpMethod.valueOf(request.getMethod());
+
+        String backendUrl = "http://100.118.20.51:8443" + request.getRequestURI().replaceFirst("/proxy", "");
+
+        HttpHeaders headers = new HttpHeaders();
+        Collections.list(request.getHeaderNames())
+                   .forEach(h -> headers.add(h, request.getHeader(h)));
+
+        HttpEntity<byte[]> entity = new HttpEntity<>(body, headers);
+
+        try {
+        	ResponseEntity<byte[]> response = restTemplate.exchange(backendUrl, method, entity, byte[].class);
+        	return ResponseEntity.status(response.getStatusCode())
+                    .headers(response.getHeaders())
+                    .body(response.getBody());
+        } catch (Exception ex) {
+        	return ResponseEntity.badRequest().build();
+        }        
+    }
+}
---
+++ proxy/src/test/java/click/scheid/proxy/ProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package click.scheid.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
