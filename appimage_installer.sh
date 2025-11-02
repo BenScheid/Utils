00c576b5b22d712e2e452a456356efea003679f5
BenScheid <bdiehcs@gmail.com>
2025-11-02 11:35:24 +0100

    remove set icon

--- appimage_installer.sh
+++ appimage_installer.sh
@@ -24,17 +24,6 @@ sudo mv "${file}" "${target_file}"
 
 sudo ln -sf "${target_file}" "/usr/local/bin/${app_name}"
 
-./"${file}" --appimage-extract
-
-icon_candidate=$(find squashfs-root -type f -iregex ".*\.\(png\|svg\|xpm\)" -iname "*icon*" | head -n 1)
-
-if [ -n "${icon_candidate}" ]; then
-	extension="${icon_candidate##*.}"
-	icon_path="${target_directory}/${app_name}/icon.${extension}"
-	sudo cp "${icon_candidate}" "${icon_path}"
-else
-	icon_path=""
-fi
 
 desktop_entry="[Desktop Entry]
 Name=${app_name}
